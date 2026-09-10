92bdc55ee4021a4b1fbd98ab788db40b5ef93250
elcaten <>
2026-09-10 14:53:47 +0200

    Reorganize Fish abbreviations

--- fish/conf.d/00-homebrew.fish
+++
@@ -1,8 +0,0 @@
-if test (uname) = Darwin
-    for brew_path in /opt/homebrew/bin/brew /usr/local/bin/brew
-        if test -x $brew_path
-            $brew_path shellenv fish | source
-            break
-        end
-    end
-end
--- fish/conf.d/10-interactive-tools.fish
+++ fish/conf.d/10-interactive-tools.fish
@@ -7,25 +7,3 @@ end
 if type -q zoxide
     zoxide init fish | source
 end
-
-abbr -a g 'git'
-
-# eza
-abbr -a ls    'eza --icons=auto'                      # List entries with icons
-abbr -a l     'eza -1 --icons=auto'                   # List one entry per line
-abbr -a ll    'eza -lah --icons=auto --git'           # Detailed list with hidden files and Git status
-abbr -a la    'eza -a --icons=auto'                   # List all entries, including hidden files
-abbr -a lt    'eza --tree --level=2 --icons=auto'     # Display a directory tree two levels deep
-abbr -a lta   'eza --tree --level=2 -a --icons=auto'  # Display a tree including hidden files
-abbr -a ld    'eza -lD --icons=auto'                  # List directories only
-abbr -a lf    'eza -lf --icons=auto'                  # List files only
-abbr -a lnew  'eza -lah --sort=modified'              # Sort by modification time, newest first
-abbr -a lsize 'eza -lah --sort=size'                  # Sort by size, largest first
-
-# tmux
-abbr --add tn  'tmux new-session -s'
-abbr --add ta  'tmux attach-session -t'
-abbr --add tl  'tmux list-sessions'
-abbr --add tk  'tmux kill-session -t'
-abbr --add tr  'tmux rename-session'
-abbr --add td  'tmux detach-client'
